f58237990d2739764b3ab1a372e5267be45b4967
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-05-06 09:40:09 +0900

    pades/index Append

---
+++ .history/src/js/utils_20230505172049.js
@@ -0,0 +1,31 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
+
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString('en-US', {
+    timeZone: "UTC",
+  })
+}
+
+export function formatBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined,
+} = {}){
+
+const filteredPosts = posts.reduce((acc, post) => {
+  const { date, draft } = post,frontmatter;
+  //filterOutDrafts if true
+  if(filterOutDrafts && draft )
+}, [])
+
+}
