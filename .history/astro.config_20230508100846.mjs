8d3ce7a051b6d36562bad00b715bcffa4d3bac52
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-05-08 10:20:19 +0900

    add new site domain and policy page css

--- .history/astro.config_20230508100846.mjs
+++ .history/astro.config_20230508100846.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://machijunblog.netlify.app',
+  site: 'https://machijunblog-.netlify.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap({
