1bd6a9d9685970d799a3b2ebcc078876c9cf258d
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-05-08 09:30:27 +0900

    add blog page post-11

---
+++ .history/astro.config_20230507172147.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import rehypeExternalLinks from 'rehype-external-links';
+
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://maastro-test',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap()],
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      }
+    }]]
+  }
+});
---
+++ .history/astro.config_20230507173622.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import rehypeExternalLinks from 'rehype-external-links';
+
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://machijunblog-astro-test.netlify.app',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap({
+    filter: (page) => page
+  })],
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      }
+    }]]
+  }
+});
