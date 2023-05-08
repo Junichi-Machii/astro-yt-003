a8576694bdeb5866ed919f8267acd1db43f4b5da
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-05-08 15:35:29 +0900

    search off

--- src/js/search.js
+++
@@ -1,7 +0,0 @@
-function buttonClick(){
-  url = 'https://www.google.co.jp/search?q="+' + nameText.value + ' site:https://www.machijunblog.netlify.app+"';
-window.open(url, '_blank')
-}
-
-let checkButton = document.getElementById('checkButton');
-checkButton.addEventListener('click', buttonClick);
