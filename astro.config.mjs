97af80f89ad89db9607831c73d960eb02669e451
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-05-21 12:41:42 +0900

    contact form add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,25 @@ import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
 import rehypeExternalLinks from 'rehype-external-links';
-
 import sitemap from "@astrojs/sitemap";
 
+
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://machijunblog.netlify.app',
-  integrations: [
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    
-    image({
+  integrations: [partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap({
     filter: page => page !== "https://machijunblog.netlify.app/about/"
-  })],
-  
-
-
+  }), tailwind()],
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, {
       content: {
@@ -33,6 +28,5 @@ export default defineConfig({
         value: ' 🔗'
       }
     }]]
-  },
-
+  }
 });
